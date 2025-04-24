e2b40f937c692d3f77004fa3570e6bae0ba3283e
zebpaa <skvorcov_90@mail.ru>
2025-04-24 13:35:40 +0400

    tried to fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "DealManager",
 	resolve: {
 		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
 	},
